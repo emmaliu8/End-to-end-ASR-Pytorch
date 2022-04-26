63c3d4a99b8f22a246a0870450b78e3c5599b891
Emma Liu <emmaliu@mit.edu>
2022-04-26 17:02:56 -0400

    Fixing reading transcriptions

--- corpus/timit.py
+++ corpus/timit.py
@@ -28,11 +28,14 @@ def read_text_for_split(file_path):
     id_to_text = {}
     with open(file_path, 'r') as f:
         content = f.read()
+        print('content')
         print(content)
         lines = content.split('\n')
-        print('new')
-        print(lines[0])
+        print('lines')
+        print(lines)
+        print(len(lines))
         for line in lines:
+            print('line')
             print(line)
             space_index = line.strip().index(' ')
             id_to_text[line[:space_index]] = line[space_index+1:]
